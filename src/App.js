c458cb870ecaf50b50a494c94e7c0b083c80f899
Chris Wingfield <wingtek2020@gmail.com>
2023-11-16 09:50:53 -0500

    app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from './assets/img/logo.png';
 
 function App() {
   return (
-   <div>Hello nucamp</div>
+   <div>
+     <Navbar dark color='primary' sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand href='/'>
+            <img src={NucampLogo} alt='nucamp logo' />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      I am ready for workshop!!!
+   </div>
   );
 }
 
