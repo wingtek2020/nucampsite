590eac9c654f73128e4b39e2a46705997c520513
Chris Wingfield <wingtek2020@gmail.com>
2023-11-21 08:09:02 -0500

    Homepage Displayed

---
+++ src/features/promotions/promotionsSlice.js
@@ -0,0 +1,14 @@
+import {PROMOTIONS} from '../../app/shared/PROMOTIONS';
+
+export const selectAllPromotions = () =>
+{
+    return PROMOTIONS;
+}
+
+export const selectFeaturedPromotion = () => {
+    return PROMOTIONS.find((promotion) => promotion.featured);
+};
+
+export const selectRandomPromotion = () => {
+    return PROMOTIONS[Math.floor(Math.random() * (PROMOTIONS.length))];
+}
