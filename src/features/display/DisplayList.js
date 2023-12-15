be7efcc05d9c406115eae7d86335110b449e065a
Christine Wingfield <wingtek2020@gmail.com>
2023-12-15 17:31:37 -0500

    added the error and loading

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -4,7 +4,8 @@ import { selectFeaturedPromotion } from "../promotions/promotionsSlice";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectFeaturedPartner } from "../partners/partnersSlice";
 import AnimatedDisplayCard from "./AnimatedDisplayCard";
-
+import Error from "../../components/Error";
+import Loading from "../../components/Loading";
 const DisplayList = () => {
   
   const items = useSelector((state) => [
@@ -17,10 +18,17 @@ const DisplayList = () => {
   return (
     <Row>
       {items.map((item, idx) => {
+        const { featuredItem, isLoading, errMsg } = item;
+        if (isLoading) {
+          return <Loading key={idx} />;
+        }
+        if (errMsg) {
+          return <Error errMsg={errMsg} key={idx} />;
+        }
         return (
-          item && (
+          featuredItem && (
             <Col md className="m-1" key={idx}>
-              <AnimatedDisplayCard item={item} />
+              <AnimatedDisplayCard item={featuredItem} />
             </Col>
           )
         );
